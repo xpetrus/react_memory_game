58806029e057ddb27085f920aa635e12dd2223e2
xpetrus <phsiao24@gmail.com>
2019-04-06 17:29:36 -0400

    Updates

---
+++ precache-manifest.08f963359cdaa8dcf54e5c9012a321d0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "a217822f995705be795f1d0c19f60d36",
+    "url": "/react_memory_game/static/media/mameta.a217822f.png"
+  },
+  {
+    "revision": "6f1983a5199e83fb57c4",
+    "url": "/react_memory_game/static/css/main.19070092.chunk.css"
+  },
+  {
+    "revision": "3e0d056f8516e20acb76",
+    "url": "/react_memory_game/static/js/runtime~main.b734728f.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react_memory_game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "c57f0c0f69e6b0a4313b7cc84b4be28c",
+    "url": "/react_memory_game/static/media/dimple.c57f0c0f.png"
+  },
+  {
+    "revision": "19c660948fe2942048a547e5341b7d31",
+    "url": "/react_memory_game/static/media/haruto.19c66094.png"
+  },
+  {
+    "revision": "c0c2278762fd1614f015eb8337477e89",
+    "url": "/react_memory_game/static/media/hideki.c0c22787.png"
+  },
+  {
+    "revision": "2e74acd904b498876a4cbf886d4f8f1c",
+    "url": "/react_memory_game/static/media/kenji.2e74acd9.png"
+  },
+  {
+    "revision": "6f1983a5199e83fb57c4",
+    "url": "/react_memory_game/static/js/main.a87ae79f.chunk.js"
+  },
+  {
+    "revision": "8af8d5928bf0384cc479bf72f51ae922",
+    "url": "/react_memory_game/static/media/mob.8af8d592.png"
+  },
+  {
+    "revision": "c80d3f3bedb1b767c7dcd57a4ef860dd",
+    "url": "/react_memory_game/static/media/musashi.c80d3f3b.png"
+  },
+  {
+    "revision": "15a6b8b18286040bdd8652544e3da923",
+    "url": "/react_memory_game/static/media/reigen.15a6b8b1.png"
+  },
+  {
+    "revision": "6e5e6efc21c7c16e90bfb9c22b654ac3",
+    "url": "/react_memory_game/static/media/ritsu.6e5e6efc.png"
+  },
+  {
+    "revision": "fab31d11ab565ae4d25c95adb343cf6b",
+    "url": "/react_memory_game/static/media/saruta.fab31d11.png"
+  },
+  {
+    "revision": "85f6ae6f1929a9b034f7b73635b54010",
+    "url": "/react_memory_game/static/media/tenga.85f6ae6f.png"
+  },
+  {
+    "revision": "450233129fbb7872fd1d79996307df62",
+    "url": "/react_memory_game/static/media/tome.45023312.png"
+  },
+  {
+    "revision": "fbe082dbe188db5b53828f7ae1901100",
+    "url": "/react_memory_game/index.html"
+  }
+];
